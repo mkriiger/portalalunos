22daa2875f1ea9957f0ecf1ec0de1b8ba24d611b
Marcio Kriiger <marciovonkriiger@gmail.com>
2026-01-12 16:36:05 -0300

    Commit inicial

---
+++ src/main/java/com/github/mkriiger/portalalunos/PortalalunosApplication.java
@@ -0,0 +1,13 @@
+package com.github.mkriiger.portalalunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalalunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalalunosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mkriiger/portalalunos/PortalalunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mkriiger.portalalunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalalunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
