36175295d05f89a34745649a5fb3aeea5a7b40bc
Marcio Kriiger <marciovonkriiger@gmail.com>
2026-01-12 17:38:32 -0300

    feat: cricao da classe base para as entidades do projeto

---
+++ src/main/java/com/github/mkriiger/portalalunos/entity/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.github.mkriiger.portalalunos.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
